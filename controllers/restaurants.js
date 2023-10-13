62c3daad39991021eaf1a595f33a0348adc1b8c5
Parsa <parsa.mostafavi.pm@gmail.com>
2023-10-13 21:53:14 +0330

    add routes

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,8 +1,67 @@
 const Restaurant = require("../models/restaurant");
 
 const getAllRestaurants = async (req, res) => {
-  const restaurants = await Restaurant.find();
-  res.status(200).send(restaurants);
+  try {
+    const restaurants = await Restaurant.find();
+    res.status(200).send(restaurants);
+  } catch (error) {
+    res.status(400).send("Invalid Request");
+  }
 };
 
-module.exports = { getAllRestaurants };
+const getRestaurant = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (!restaurant) {
+      return res.status(404).send("Restaurant Doesnt Found");
+    }
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(400).send("Invalid Id");
+  }
+};
+
+const deleteRestaurant = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
+    if (!restaurant) {
+      return res.status(404).send("Restaurant Doesnt Found");
+    }
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(400).send("Invalid Id");
+  }
+};
+
+const updateRestaurant = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!restaurant) {
+      return res.status(404).send("Restaurant Doesnt Found");
+    }
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(400).send("Invalid Id");
+  }
+};
+
+const addRestaurant = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.create(req.body);
+    res.status(200).send(restaurant);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+module.exports = {
+  getAllRestaurants,
+  getRestaurant,
+  deleteRestaurant,
+  updateRestaurant,
+  addRestaurant,
+};
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllRestaurants } = require("../controllers/restaurants");
+const {
+  getAllRestaurants,
+  getRestaurant,
+  deleteRestaurant,
+  updateRestaurant,
+  addRestaurant,
+} = require("../controllers/restaurants");
 
-router.route("/").get(getAllRestaurants);
+router.route("/").get(getAllRestaurants).post(addRestaurant);
+router
+  .route("/:id")
+  .get(getRestaurant)
+  .delete(deleteRestaurant)
+  .patch(updateRestaurant);
 
 module.exports = router;
